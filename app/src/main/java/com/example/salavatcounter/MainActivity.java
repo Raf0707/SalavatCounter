2a36efec463337d0181ee9050b62fe278e6b92e2
Raf <Raf_0707@mail.ru>
2021-12-06 17:33:21 +0300

    binding the target to the ProgressBar statusBar.show()

--- app/src/main/java/com/example/salavatcounter/MainActivity.java
+++ app/src/main/java/com/example/salavatcounter/MainActivity.java
@@ -29,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
     private ProgressBar mProgressBar;
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
